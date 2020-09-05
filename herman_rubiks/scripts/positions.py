38bf2512df0eb196aa37a1f94ecd6c107137e42b
Dave Niewinski <davesarmoury@gmail.com>
2020-09-05 15:57:08 -0400

    minor changes

--- herman_rubiks/scripts/positions.py
+++ herman_rubiks/scripts/positions.py
@@ -2,12 +2,26 @@ from moveit_commander.conversions import list_to_pose
 import copy
 
 approach_height = 0.05
+approach_offset = 0.05
 
 positions = {}
 positions["back"] = list_to_pose([0, 0, 0, 0, 0, 0])
+positions["back_up"] = list_to_pose([0, 0, approach_height, 0, 0, 0])
+positions["back_out"] = list_to_pose([-approach_offset, 0, 0, 0, 0, 0])
+positions["back_out_up"] = list_to_pose([-approach_offset, 0, approach_height, 0, 0, 0])
 positions["front"] = list_to_pose([0, 0, 0, 0, 0, 3.14159])
+positions["front_up"] = list_to_pose([0, 0, approach_height, 0, 0, 3.14159])
+positions["front_out"] = list_to_pose([approach_offset, 0, 0, 0, 0, 3.14159])
+positions["front_out_up"] = list_to_pose([approach_offset, 0, approach_height, 0, 0, 3.14159])
 positions["left"] = list_to_pose([0, 0, 0, 0, 0, 1.5707])
+positions["left_up"] = list_to_pose([0, 0, approach_height, 0, 0, 1.5707])
+positions["left_out"] = list_to_pose([0, -approach_offset, 0, 0, 0, 1.5707])
+positions["left_out_up"] = list_to_pose([0, -approach_offset, approach_height, 0, 0, 1.5707])
 positions["right"] = list_to_pose([0, 0, 0, 0, 0, -1.5707])
+positions["right_up"] = list_to_pose([0, 0, approach_height, 0, 0, -1.5707])
+positions["right_out"] = list_to_pose([0, approach_offset, 0, 0, 0, -1.5707])
+positions["right_out_up"] = list_to_pose([0, approach_offset, approach_height, 0, 0, -1.5707])
+
 home = [-1.347628694484477, -1.6605963965156643, -1.9764216397655545, -2.645967955802357, 0.0, 0.0]
 
 def make_moves(arm, positions):
@@ -28,20 +42,22 @@ def go_home(arm):
     arm.go(home)
     arm.stop()
 
+def get_pick_moves(side):
+    moves = []
+    moves.append(positions[side + "_out_up"])
+    moves.append(positions[side + "_out"])
+    moves.append(positions[side])
+    moves.append(positions[side + "_up"])
+    return moves
+
 def pick_rubik(arm, side):
     go_home(arm)
-    moves = []
-    arm.set_pose_reference_frame("fixture")
-    moves.append(positions[side])
+    moves = get_pick_moves(side)
     make_moves(arm, moves)
 
 def place_rubik(arm, side):
     go_home(arm)
-    moves = []
-    moves.append(positions[side + "_up"])
-    moves.append(positions[side + ""])
-    moves.append(positions[side + "_pounce"])
-    moves.append(positions[side + "_pounce_up"])
+    moves = get_pick_moves(side).reverse()
     make_moves(arm, moves)
 
 def turn_clockwise(arm):
